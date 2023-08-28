6fa53b8da6e34a9d9fd62e1be984d5afc3abc0fd
junggji <calrinar@gmail.com>
2023-08-28 11:43:17 +0900

    first

---
+++ src/ch05reference/C02reference.java
@@ -0,0 +1,25 @@
+package ch05reference;
+
+public class C02reference {
+	public static void main(String[] args) {
+		// 참조타입
+		// 기본타입 8개 빼고 모두 다
+		
+		String a; // 참조타입
+		String b; // 참조타입
+		
+		int c; // 기본타입
+		int[] d; // 참조타입
+		
+		String e;
+		e = "java"; // --> 변수에 인스턴스가 있는 주소(memory address)가 저장된다. -> 그래서 참조타입
+		
+		int f;
+		f = 9;
+		
+		
+		
+		
+		
+	}
+}
---
+++ src/ch05reference/C05array.java
@@ -0,0 +1,17 @@
+package ch05reference;
+
+public class C05array {
+	public static void main(String[] args) {
+		
+		int[] a = {3,4,5};
+		int[] b = a;
+		
+		int[] c = {10, 20, 30};
+		int[] d = {10, 20, 30};
+		
+		
+		
+		
+		
+	}
+}
