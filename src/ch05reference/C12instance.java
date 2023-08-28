fa5528e310ac978eaab5a82057fd947e16c667a8
junggji <calrinar@gmail.com>
2023-08-28 15:33:44 +0900

    객체와 클래스

---
+++ src/ch05reference/C12instance.java
@@ -0,0 +1,13 @@
+package ch05reference;
+
+public class C12instance {
+    public static void main(String[] args) {
+        // instance
+        // object
+        // 객체 :
+        // 1. 속성(값, 필드, property, attribute)
+        // 2. 기능(메소드, 함수, 액션) --- 기능에 의해서 속성이 바뀔수도 있다!
+        // 클래스는 객체를 위한 설계도 이므로, 클래스에는 필드와 메소드가 명시되어 있어야 한다는 것. + 생성자
+
+    }
+}
