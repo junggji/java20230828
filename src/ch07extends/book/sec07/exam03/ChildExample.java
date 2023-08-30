46cf93b83e7778fdd38e6c6a42b36e091f69c4a5
junggji <calrinar@gmail.com>
2023-08-30 14:00:34 +0900

    다형성

--- src/ch07extends/book/sec07/exam03/ChildExample.java
+++ src/ch07extends/book/sec07/exam03/ChildExample.java
@@ -2,14 +2,14 @@ package ch07extends.book.sec07.exam03;
 
 public class ChildExample {
     public static void main(String[] args) {
-        Parent parent = new Child();
+        Parent parent = new Child();        // 타입이 Parent
 
         parent.field1 = "data1";
         parent.method1();
         parent.method2();
 
 //        parent.field2 = "data2";    // (불가능)
-//        parent.method3();           // 불가능
+//        parent.method3();           // 불가능 --> Child에서만 있는 메소드 이므로
 
         Child child = (Child)parent;
         child.field2 = "data2";         //가능
---
+++ src/ch07extends/lecture/C14type.java
@@ -0,0 +1,14 @@
+package ch07extends.lecture;
+
+public class C14type {
+    public static void main(String[] args) {
+        // 기본타입
+        int a = 3;
+
+        String b = "java";
+
+        Object o = "java";
+    }
+}
+class Parent {}
+class Child extends Parent{}
